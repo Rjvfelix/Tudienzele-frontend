78ecb5582d7a4815057358169107096db91cef0a
RJVFELIX-PREDATOR <rjvfelix@gmail.com>
2025-06-03 15:33:30 +0200

    feat: export BeneficiaryProfileComponent and update AppModule imports

--- src/app/master-list/beneficiary-profile.component.ts
+++ src/app/master-list/beneficiary-profile.component.ts
@@ -1,11 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-beneficiary-profile',
   templateUrl: './beneficiary-profile.component.html',
   styleUrls: ['./beneficiary-profile.component.css']
 })
-export class BeneficiaryProfileComponent {
+export class BeneficiaryProfileComponent implements OnChanges {
   @Input() beneficiary: any;
+
+  ngOnChanges() {
+    console.log('BeneficiaryProfileComponent beneficiary:', this.beneficiary);
+  }
+
+  getOtherKeys(obj: any): string[] {
+    if (!obj) return [];
+    const known = [
+      'id', 'codeBeneficiary', 'firstname', 'postname', 'lastname',
+      'menage', 'isHead', 'liaisonChefMenage', 'contact', 'sexe',
+      'isNaissanceKnown', 'dateNaissance', 'age', 'statusMatrimonial',
+      'education', 'handicap', 'agr', 'serviceFinaciere', 'observations',
+      'logaltoCode', 'createdBy', 'createdDate', 'lastModifiedBy', 'lastModifiedDate',
+      'deleted', 'synced'
+    ];
+    return Object.keys(obj).filter(k => known.indexOf(k) === -1);
+  }
 }
 
